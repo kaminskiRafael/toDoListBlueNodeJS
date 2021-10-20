174be7cde44a2de4a5b1ce08b38089460b41c744
kaminskiRafael <rafael@rfkdev.com.br>
2021-10-19 23:12:13 -0300

    conserto cors novo

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ if(process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const cors = require('cors');
 
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200,
-}
+// const corsOptions = {
+//     origin: 'http://localhost:3000',
+//     optionsSuccessStatus: 200,
+// }
 
 const app = express();
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(express.json());
 
 const Connection = require('./connection/connection');
